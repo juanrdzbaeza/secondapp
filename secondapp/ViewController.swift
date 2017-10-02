75e39a71b4c96ea1e140b1d8c82d953df1632173
Juan Rodríguez Baeza <juanrdzbaeza@gmail.com>
2017-10-02 19:03:57 +0200

    aplicacion completa hasta antes de los ejercicios

--- secondapp/ViewController.swift
+++ secondapp/ViewController.swift
@@ -8,18 +8,90 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //MARK: properties
+    @IBOutlet weak var nombreLabel: UILabel!
+    @IBOutlet weak var nombreTxt: UITextField!
+    @IBOutlet weak var imagen: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        nombreTxt.delegate    = self;
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: Actions
+    @IBAction func botonReset(sender: UIButton) {
+        nombreLabel.text    = "Hola desconocido"
+        
+    }
+    
+   @IBAction func seleccionarImagen(sender: UITapGestureRecognizer) {
+        nombreTxt.resignFirstResponder()
+        let imagePickerCtrl         = UIImagePickerController()
+        imagePickerCtrl.sourceType  = .PhotoLibrary
+        imagePickerCtrl.delegate    = self
+        presentViewController(imagePickerCtrl, animated:true, completion:nil)
+    }
 
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool{
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        if nombreTxt.text != "" {
+            nombreLabel.text = "Hola "+nombreTxt.text!
+        }else{
+            nombreLabel.text = "Hola desconocido"
+        }
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let selectedImage   = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imagen.image        = selectedImage
+        dismissViewControllerAnimated(true, completion: nil)
+        
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
