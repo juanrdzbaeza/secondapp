5500bf4ec886f06a4011e1f81cbbe212247caddf
Juan Rodríguez Baeza <juanrdzbaeza@gmail.com>
2017-10-09 16:39:31 +0200

    Nueva rama para la Sesion03

---
+++ EvaluationControl.swift
@@ -0,0 +1,36 @@
+//
+//  EvaluationControl.swift
+//  secondapp
+//
+//  Created by Juan Rodríguez Baeza on 9/10/17.
+//  Copyright © 2017 Juan Rodríguez Baeza. All rights reserved.
+//
+
+import UIKit
+
+class EvaluationControl: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    // MARK: inicializacion
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
