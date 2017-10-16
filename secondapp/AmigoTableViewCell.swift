7c056fe358e43dcdd8f44256df510d4e7b399477
Juan Rodríguez Baeza <juanrdzbaeza@gmail.com>
2017-10-16 17:09:42 +0200

    Comienzo de Sesion04

---
+++ secondapp/AmigoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AmigoTableViewCell.swift
+//  secondapp
+//
+//  Created by Juan Rodríguez Baeza on 16/10/17.
+//  Copyright © 2017 Juan Rodríguez Baeza. All rights reserved.
+//
+
+import UIKit
+
+class AmigoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
